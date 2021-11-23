26618689c43e3138402c4215d60ce6f6ff329c20
Dylan Lamont <dylanlamont3840@gmail.com>
2021-11-23 20:13:44 +1100

    Inits project

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("robot", () => {
+  it.todo("can be placed");
+  it.todo("can move");
+  it.todo("can rotate");
+  it.todo("should not fall off the table");
+  it.todo("can be placed multiple times");
+  it.todo("can log movement");
 });
